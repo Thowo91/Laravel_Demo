4aabe57f15fc45486a9f6c1752f24cad20eb9481
Thomas Worofsky <Thomas.Worofsky@gmail.com>
2019-10-10 09:55:50 +0200

    Vue Input List

---
+++ resources/js/components/backend/ExcampleInputList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <h3>Input List</h3>
+        <ul>
+            <li v-for="todo in items">{{todo}}</li>
+        </ul>
+
+        <input type="text" v-model="item">
+        <button @click="items.push(item)" >Add Item</button>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data: function() {
+            return {
+                item: '',
+                items: []
+            }
+        }
+    }
+</script>
--- resources/views/backend/exampleVue.blade.php
+++ resources/views/backend/exampleVue.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts.backend')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-6">
+            <example-input-list></example-input-list>
+        </div>
+    </div>
+
+@endsection
