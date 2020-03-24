4677fa924bde646c4cd5fe191ba3a0ce960d3904
Thomas Worofsky <Thomas.Worofsky@gmail.com>
2020-03-24 14:38:12 +0100

    Test Translation-loader

--- app/Http/Controllers/Frontend/LanguageController.php
+++ app/Http/Controllers/Frontend/LanguageController.php
@@ -7,10 +7,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Input;
 use Session;
+use Spatie\TranslationLoader\LanguageLine;
 
 class LanguageController extends Controller
 {
     public function changeLanguage() {
+
+//        LanguageLine::query()->updateOrCreate([
+//            'group' => 'test',
+//            'key' => 'greeting',
+//            'text' => ['fr' => 'bonjour', 'de' => 'Servus', 'en' => 'Hello']
+//        ]);
+
+//        $line = LanguageLine::query()
+//            ->where('key', 'greeting')
+//            ->first();
+//        dd($line);
+//
+//        $line->setTranslation('en', 'test');
+//        $line->setTranslation('de', 'halali');
+//        $line->save();
+
 
         $language = Input::get('lang');
         Session::put('language',$language);
